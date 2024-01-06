d3649d69828fb524a039370f579400c915cf0d5c
paper-roe <tszabo.tom@gmail.com>
2024-01-06 16:28:43 +1100

    title screen
    - added title text
    - added input box for player name
    - added buttons to set and display name

---
+++ Assets/Scripts/TitleScene/TitleScreenManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TitleScreenManager : MonoBehaviour
+{
+    [SerializeField] private TMP_InputField playerNameInput;
+    public string playerName { get; private set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetName();
+        DisplayName();
+    }
+
+    public void SetName()
+    {
+        playerName = playerNameInput.text;
+    }
+
+    public void DisplayName()
+    {
+        Debug.Log(playerName);
+    }
+}
